9dfe05978e209a318ddcb900260883e68f7cfcee
arcum-omni <travis.l.eiler@gmail.com>
2020-01-15 17:32:11 -0800

    Created Test Code To Verify Functionality

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -10,7 +10,23 @@ namespace NestedObjects
     {
         static void Main(string[] args)
         {
+            Student s = new Student();
+            s.FullName = "travis";
+            s.FullName = s.FullName.ToUpper();
 
+            Course cpw212 = new Course()
+            {
+                CourseID = 1,
+                CourseTitle = "Adv .NET",
+                Credits = 5,
+                Description = "Advanced C#"
+            };
+
+            s.Schedule.Add(cpw212);
+
+            List<Course> courses = s.Schedule;
+            courses.Add(cpw212);
+            courses.Add(cpw212);
         }
     }
 }
